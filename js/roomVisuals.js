380d8946a2b24f549fabd35c6abd29ad44b54581
sharkvelocity <71565691+sharkvelocity@users.noreply.github.com>
2025-07-28 22:21:43 -0700

    Create roomVisuals.js

---
+++ js/roomVisuals.js
@@ -0,0 +1,15 @@
+// === Room visuals used to determine background image per room/direction ===
+export const roomVisuals = {
+  Van: { N: "img/Van_N.png" },
+  Foyer: { N: "img/Foyer_N.png" },
+  LivingRoom: { N: "img/LivingRoom_N.png" },
+  Kitchen: { N: "img/Kitchen_N.png" },
+  DiningRoom: { N: "img/DiningRoom_N.png" },
+  Bathroom: { N: "img/Bathroom_N.png" },
+  Basement: { N: "img/Basement_N.png" },
+  Garage: { N: "img/Garage_N.png" },
+  MasterBedroom: { N: "img/MasterBedroom_N.png" },
+  KidsRoom: { N: "img/KidsRoom_N.png" },
+  Hallway: { N: "img/Hallway_N.png" },
+  Utility: { N: "img/Utility_N.png" }
+};
