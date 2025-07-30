c4f83adad8fff55503579e6e02cd52ae12ebebb7
sharkvelocity <71565691+sharkvelocity@users.noreply.github.com>
2025-07-30 10:36:07 -0700

    Update vanMonitor.js

--- js/vanMonitor.js
+++ js/vanMonitor.js
@@ -6,7 +6,7 @@
 import { game } from "./state.js";
 import { logToGame, showNotebookUpdateBadge } from "./ui.js";
 import { ghostProfiles, roomVisuals } from "./state.js";
-import { playAudio } from "./audioManager.js";
+import { monitor_boot } from "./audioManager.js";
 
 let vanMonitorBooted = false;
 let vanMonitorInterval = null;
